700e8fa28cebfbdb3db3df4b8c3abc5cddbd6846
whyohwhyoh <70037408+whyohwhyoh@users.noreply.github.com>
2020-08-29 13:42:50 -0400

    Update ensamblaje.js

--- ensamblaje.js
+++ ensamblaje.js
@@ -1,9 +1,7 @@
-// AGREGANDO FOTO
-
-
-
 window.onload = function () {
   const hydra = new Hydra()
+
+s1.initCam(s1)
 
 a.show();
 a.setBins(4);
@@ -16,7 +14,7 @@ a.setSmooth(.96)
   video.crossOrigin = "anonymous"
   video.muted = true
   video.loop = true
-  video.src = "./fotos/videos/explosiones_slower.mp4"
+  video.src = "./fotos/videos/explosiones_slow.mp4.mov"
 
   video.play().then(()=>s0.init({src:video, dynamic:true}))
 
@@ -32,15 +30,15 @@ a.setSmooth(.96)
 
   osc(({time})=>4+3*Math.sin(time*.05),.02)
   	// .modulateRepeat(o0)
-    .scale(  () => a.fft[1]*2 )
+    .scale(  () => a.fft[1]*2.5+.5 )
 
   	 .rotate (({time})=> time*.01)
-    .rotate (() => mouse.y *.0008 + 5)
+    .rotate (() => mouse.y *.008 + 1.5)
   	.blend(o1,.25)
   .out(o1)
 
 
-  osc(3,.7,() => mouse.x *.8 )
+  osc(3,.7,() => mouse.x *.008 )
   	.kaleid()
     .mask(o0)
   	// .thresh()
@@ -61,37 +59,7 @@ src(s0)
 
 .out(o3)
 
-  
-render(o3)
+  render(o3)
 
   speed=.15
 
-
-
-
-// OLIVIA'S FEEDBACK
-
-// osc(10, 0.1, 0.8)
-//   .rotate(0, 0.1)
-//   .kaleid()
-//   .color(-1, 1)
-// .out(o0)
-//
-//
-//
-// src(s0)
-//   .color(-1, Math.random()*2, 1)
-//   .colorama()
-// .out(o1)
-//
-//
-// src(o1)
-//        .layer(src(o1).mask(shape(4, 0.4, 0)))
-//        .scrollX([0.005, -0.005])
-//        .scrollY(0.005)
-// .out(o2)
-//
-//      render(o1)
-
-
-}
