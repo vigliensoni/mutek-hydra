7e830657da0a6d115d1565d5ffd52dcdfdf75423
Eduardo  Perez <Guayo@Eduardos-MacBook-Pro.local>
2020-09-14 23:38:24 -0400

    Update window.js

--- window.js
+++ window.js
@@ -7,7 +7,7 @@ window.onload = function () {
 a.show();
 a.setBins(4);
 a.setSmooth(.96)
-// a.hide()
+a.hide()
 
 
   const video = document.createElement("video")
@@ -18,16 +18,6 @@ a.setSmooth(.96)
   video.src = "./fotos/videos/cross_section.mp4"
 
   video.play().then(()=>s0.init({src:video, dynamic:true}))
-
-
-
-// src(s0)
-// .saturate(0)
-// // .brightness(.16)
-// .rotate(()=>a.fft[1]*1.2, .01)
-// .contrast(3)
-// .invert()
-// .out(o0)
 
 
 
@@ -43,10 +33,10 @@ solid(0)
   .rotate(04, 0.4)
   .color(1,3,4)
   .color( ()=>Math.sin(time)*0.002+.28,0.2,0.2 )
-  .modulateRotate( osc(()=>a.fft[2]*3+5, 0.2).color(6), 6)
+  .modulateRotate( osc(()=>a.fft[3]*3+5, 0.2).color(6), 6)
   // .modulateRotate( osc(5, 0.2).color(6), 6)
 
-  .modulateScale( noise(4, ()=>a.fft[0]*0.3+.02), 4))
+  .modulateScale( noise(4, ()=>a.fft[0]*4+.02), 4))
 
   // .modulateScale( noise(4, 0.1), 4))
 
@@ -59,20 +49,22 @@ solid(0)
 .out(o1)
 
 
-// voronoi(8,1)
-  noise(()=> a.fft[2]*3+120,0)
-// voronoi(()=> a.fft[0]*1.2+2, 1)
+ // voronoi(8,1)
+  // noise(()=> a.fft[2]*3+120,0)
+ voronoi(()=>mouse.x * .5,()=> a.fft[2]*3+90,0)
 
-.mult(osc(10,0.1,()=>Math.sin(time)*.3+.14).saturate(3).kaleid(200))
+
+.mult(osc(3,()=> a.fft[2]*3+0.1,()=>Math.sin(time/15)*.3+.08).saturate(4).kaleid(200))
 .modulate(o1,0.5)
 .add(o1,0.8)
 .scrollY(-0.1)
 .scale(0.99)
 .modulate(o1)
+.modulate(noise(()=>Math.sin(time)*.07+.2),()=>Math.sin(time)*.02+.1)
 
 // .modulate(o0)
 
-// .modulate(voronoi(8,1),0.008)
+//.modulate(voronoi(8,1),0.008)
 // .luma(0.3)
 .blend(o2)
 .blend(o2).blend(o2)
@@ -86,9 +78,6 @@ solid(0)
 
 
 render(o2)
-
-
-
 
 
 
