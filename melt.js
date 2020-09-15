c0a8c64263ae8859b7fc4cb247dafc4ea27dfd3d
Eduardo  Perez <Guayo@Eduardos-MacBook-Pro.local>
2020-09-15 00:12:04 -0400

    Update melt.js

--- melt.js
+++ melt.js
@@ -21,54 +21,45 @@ a.setSmooth(.96)
 
 
 
-src(s0)
-.saturate(.4)
-// .modulateScale(o0,0.02)
-.contrast(()=>mouse.x * .0008+.6 )
- .rotate(()=>a.fft[2]*4)
-.scale(()=>a.fft[1]*.24+1)
-// .kaleid(2)
-// .modulate(o0, 0.01)
-.rotate(({time})=>5.0+2*Math.sin(time*.03),.01)
-.modulate((o2), ()=> a.ftt[0]*1.2)
 
-.blend(o0)
-// .(.2)
-
-
-.out(o0)
+  src(s0)
+  .saturate(.4)
+  // .modulateScale(o0,0.02)
+  .contrast(()=>mouse.x * .0008+.6 )
+  .rotate(()=>a.fft[2]*4)
+  .scale(()=>a.fft[1]*.24+1)
+  // .kaleid(2)
+  // .modulate(o0, 0.01)
+  .rotate(({time}) => 5.0+2*Math.sin(time*.03),.01)
+  // .scale(({time})=>Math.sin(time*1)*0.5+1)
+  .modulate((o2), () => a.fft[0]*1.2)
+  .blend(o0)
+  .out(o0)
 
 
 
-voronoi(2,0.3,0.2).shift(0.5)
-.modulatePixelate(voronoi(4,0.2),32,2)
-.scale(()=>1+(Math.sin(time*12.5)*0.05),()=>mouse.y * 0.002)
-.out(o1)
+  voronoi(2,0.3,0.2).shift(0.5)
+  .modulatePixelate(voronoi(4,0.2),32,2)
+  .scale(()=>1+(Math.sin(time*12.5)*0.05),()=>mouse.y * 0.002)
+  .out(o1)
 
 
 
-osc(4, ()=> a.fft[0]*.2-.1, 1)
-.scrollY( ()=>0.5 + mouse.y * 0.0014,.6,1)
-.out(o2)
+  osc(4, ()=> a.fft[0]*.2-.1, 1)
+  .scrollY( ()=>0.5 + mouse.y * 0.0014,.6,1)
+  .out(o2)
 
 
+  src(o0)
+
+  .mult(o2,.8)
+  .modulateRotate(o0,()=>a.fft[1]*.1+1)
+  .invert()
+  .modulateScrollY(o1)
+  .out(o3)
+
+  render(o3)
 
 
-
-
-
-src(o0)
-
-.mult(o2,.8)
-.modulateRotate(o0,()=>a.fft[1]*.1+1)
-.invert()
-.modulateScrollY(o1)
-
-.out(o3)
-
-
-
-
-render(o3)
 
 }
