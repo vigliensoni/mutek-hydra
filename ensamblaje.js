aee676c68d1857b6f711f1896f96d21b5944a75a
Eduardo  Perez <Guayo@Eduardos-MacBook-Pro.local>
2020-09-15 00:24:06 -0400

    Update ensamblaje.js

--- ensamblaje.js
+++ ensamblaje.js
@@ -20,6 +20,8 @@ a.setSmooth(.96)
 
 
   osc(({time})=>5.0+2*Math.sin(time*.03),.02)
+    .scrollX(() => a.fft[1]*4+5 )
+
   	.modulateRepeat(o1)
   	.modulateScale(osc(2,4))
   	.blend(o0)
@@ -29,7 +31,8 @@ a.setSmooth(.96)
 
   osc(({time})=>4+3*Math.sin(time*.05),.02)
   	// .modulateRepeat(o0)
-    .scale(  () => a.fft[1]*2.5+.5 )
+    .scale(  () => a.fft[0]*2.5+.5 )
+    .scrollX(() => a.fft[3]*-.1+.2 )
 
   	 .rotate (({time})=> time*.01)
     .rotate (() => mouse.y *.008 + 1.5)
@@ -48,19 +51,25 @@ a.setSmooth(.96)
 
 
 src(s0)
-  .scale(1.7)
+  .scale(() => a.fft[1]*.8+1.3)
   .scrollX(({time})=>4+3*Math.sin(time*.2))
+
   .saturate(0.2)
   .contrast(2)
-
-  .mult(o2,.9)
   .modulate(s0, .08)
 
+  .mult(o2,()=>mouse.y*.001+.2)
+  .modulate(s0, .03)
+
 .out(o3)
+
+
 
 render(o3)
 
 speed=.15
+
+
 
 
 
