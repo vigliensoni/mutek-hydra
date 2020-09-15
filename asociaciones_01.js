acd8f7d2f2b69c429d1aedff38eaa04d45c0961f
Eduardo  Perez <Guayo@Eduardos-MacBook-Pro.local>
2020-09-15 00:21:12 -0400

    Update asociaciones_01.js

--- asociaciones_01.js
+++ asociaciones_01.js
@@ -3,10 +3,11 @@ window.onload = function () {
   const hydra = new Hydra()
 
 
-a.show();
-a.setBins(4);
-a.setSmooth()
-// a.hide()
+
+    a.show();
+    a.setBins(4);
+    a.setSmooth(.96)
+    a.hide()
 
 
   const video = document.createElement("video")
@@ -18,78 +19,44 @@ a.setSmooth()
 
   video.play().then(()=>s0.init({src:video, dynamic:true}))
 
-  // const video2 = document.createElement("video")
-  // video2.autoplay = true
-  // video2.crossOrigin = "anonymous"
-  // video2.muted = true
-  // video.loop = true
-  // video.src = "./fotos/videos/explosiones_slow.mp4.mov"
-  // video.play().then(()=>s1.init({src:video2, dynamic:true}))
-  //
-  //
 
 
-  a.show();
-  a.setBins(4);
-  a.setSmooth(.96)
-  // a.hide()
 
-
-  // osc(23,.004,.3)
-  // .scrollX(.2)
-  // .rotate(.25,.2)
-  // .modulateRotate(o0,() => 0.1 + mouse.x * 0.0002)
-  // .modulateScale(o1)
-  // .blend(o0)
-  // .out(o0)
-
-  osc(13,()=> a.fft[4]*.03 )
-
+  osc(3,()=> a.fft[4]*.03 )
   .rotate(1.4,.2)
   .add(osc(3,.2))
   .out(o0)
 
 
 
-  // osc(8,()=> a.fft[0]*.00003)
-  //   .rotate(1.4,.2)
-  //
-  // .color(.9,0,5)
-  // .modulate(osc(10).rotate(1, 0.5))
-  // .rotate(1, 0.2)
-  // //   .add(osc(3,.2))
-  // .modulate(o0)
-  // .out(o1)
+
+  osc(0.50   ,1.25).mult(shape(12,20.09).rotate(()=>Math.random()*15))
+    .diff(osc(()=>a.fft[0]*.1+03))
+    .add(shape(3,22).rotate(()=>Math.random()*15).blend(gradient(1.6)))
+    .modulate(noise(6)
+              .modulate(noise(2).scrollY(()=> a.fft[2]*5,0.0625)))
+              // .modulate(osc(3).rotate(()=>4+3*Math.sin(time*.000020)))
+    .blend(o0)
+    .color(17,-90.5,0.75)
+    .out(o1)
 
 
-
-
-
-  shape(3,0.125,.02)
-
-  .rotate(()=> a.fft[4]*3+2 )
-  .scrollX([-0.5,-0.2,0.3,-0.1,-0.1].smooth(0.1).fast(0.3))
-  .scrollY([0.25,-0.2,0.3,-0.1,0.2].smooth(0.9).fast(0.15))
-  .modulateRepeat(osc(60,-0.015,0.13)
-                  .rotate(Math.PI/2))
-  . modulateScale(osc(()=> a.fft[2]*7),.4)
-
-  // .modulateRepeat(o0)
-  .out(o1)
 
 
 
   src(s0)
-    .scale(.7)
-    .modulate(o3)
-    .scale(() => .7 + mouse.x * 0.0002)
+    // .modulate(o3)
+    .scale(() =>  mouse.x * 0.0005+ .2)
+    .rotate(()=>a.fft[3]*.01*Math.sin(time*.2)+.003)
     .modulate(o1)
-    .kaleid(()=>mouse.y*.002+1)
+    .rotate(()=> a.fft[0]*-.3+-.3 )
+
+    .kaleid(()=>mouse.y*.001+1)
     .scrollX(({time})=>4+3*Math.sin(time*.002))
     .saturate(0.2)
     .contrast(2)
-   .modulateRotate(o0,.4, .2)
-   .modulate(o3)
+    .modulateRotate(noise(.2, ()=> a.fft[3]*-.02 ))
+   .modulate(o2,.002)
    .blend(o3)
 
 .out(o2)
@@ -97,19 +64,14 @@ a.setSmooth()
 
 
 
-gradient(.2,.3,.2)
-.saturate(()=>a.fft[1]*7)
+gradient(.2,.03,2)
+.saturate(()=>a.fft[1]*3+0.2)
 .mask(o1)
 
 .out(o3)
 
 
-
-
-
-  render(o2)
-
-
+render(o2)
 
 
 
